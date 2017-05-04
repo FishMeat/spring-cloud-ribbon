fdc428826e1d0636b883571a1bebf176da89c7d6
sggb <shangggb@joyowo.com>
2017-05-04 14:09:53 +0800

    增加配置

---
+++ src/main/java/com/fishmeat/Application.java
@@ -0,0 +1,18 @@
+package com.fishmeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author sggb
+ * @since 04.05.2017
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
