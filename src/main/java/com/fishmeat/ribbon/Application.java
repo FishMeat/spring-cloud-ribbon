81cab0590b47cc1f78d43cf9aac350651af43cee
sggb <shangggb@joyowo.com>
2017-05-16 13:25:07 +0800

    修改包路径

--- src/main/java/com/fishmeat/ribbon/Application.java
+++ src/main/java/com/fishmeat/ribbon/Application.java
@@ -1,4 +1,4 @@
-package com.fishmeat;
+package com.fishmeat.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/fishmeat/ribbon/RibbonService.java
+++ src/main/java/com/fishmeat/ribbon/RibbonService.java
@@ -1,4 +1,4 @@
-package com.fishmeat;
+package com.fishmeat.ribbon;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
